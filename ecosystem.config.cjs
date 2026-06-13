5c62ad1d014ec23a59d507574f976258aa283244
aipmy <aippmy@gmail.com>
2026-06-14 03:15:34 +0700

    feat: expose PORT env option in ecosystem.config.cjs for easier PM2 deployment port configurations

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       script: "backend/src/server.js",
       interpreter: "node",
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 4200
       },
       max_memory_restart: "512M",
       restart_delay: 3000,
