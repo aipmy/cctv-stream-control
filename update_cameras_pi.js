4565d00172d993377c1bf5d5cc387bb8552cbaf7
aipmy <aippmy@gmail.com>
2026-07-14 03:10:57 +0700

    chore: remove temporary scripts

--- update_cameras_pi.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-const path = './backend/data/cameras.json';
-const data = JSON.parse(fs.readFileSync(path, 'utf8'));
-data.forEach(cam => {
-  let modes = cam.detectionModes || ["pixel", "human", "pet"];
-  cam.detectionModes = modes.filter(m => m !== "pixel");
-});
-fs.writeFileSync(path, JSON.stringify(data, null, 2));
-console.log('Updated cameras.json on Pi');
