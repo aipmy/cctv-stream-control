fa16427e3221b075c6de3e1428c5f9a15fae117e
aipmy <aippmy@gmail.com>
2026-07-22 06:54:46 +0700

    fix(recording): remove invalid HTTP reconnect flags from RTSP FFmpeg input args

--- backend/src/stream/streamManager.js
+++ backend/src/stream/streamManager.js
@@ -153,8 +153,8 @@ async function startRecording(camera) {
 
   const args = [
     "-hide_banner", "-loglevel", "error",
-    "-reconnect", "1", "-reconnect_at_eof", "1", "-reconnect_streamed", "1", "-reconnect_delay_max", "5",
     "-rtsp_transport", "tcp",
+    "-stimeout", "5000000",
     "-use_wallclock_as_timestamps", "1",
     "-i", go2rtcInput,
     "-sn", "-dn"
