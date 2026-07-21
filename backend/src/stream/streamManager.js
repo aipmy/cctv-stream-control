e359bd8ad60c21096bb4c7a9bed66c377e44a4d3
aipmy <aippmy@gmail.com>
2026-07-22 06:55:24 +0700

    fix(recording): remove unsupported stimeout parameter to allow FFmpeg recording to run continuously

--- backend/src/stream/streamManager.js
+++ backend/src/stream/streamManager.js
@@ -154,7 +154,6 @@ async function startRecording(camera) {
   const args = [
     "-hide_banner", "-loglevel", "error",
     "-rtsp_transport", "tcp",
-    "-stimeout", "5000000",
     "-use_wallclock_as_timestamps", "1",
     "-i", go2rtcInput,
     "-sn", "-dn"
